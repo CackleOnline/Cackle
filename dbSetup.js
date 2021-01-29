d969a6d64462615b613f23b02c566eb2e54eb031
Kiwiscripter <ronin@kiwiscripter.xyz>
2021-01-29 13:46:13 -0600

    updated dbSetup for new auth stuff

--- dbSetup.js
+++ dbSetup.js
@@ -1,9 +1,10 @@
 var r = require('rethinkdb')
-    ;
+
 const rethinkDbConnectionObject = {
     host: 'localhost',
     port: 28015
 };
+
 r.connect(rethinkDbConnectionObject, (err, conn) => {
 
     if (err) {
@@ -24,9 +25,25 @@ r.connect(rethinkDbConnectionObject, (err, conn) => {
     })
 
     //Create test post
-    r.table('posts').insert({ title: "hello", content: "test content", author: "testuser", timestamp: 1234567 }).run(conn, function(err, result) {
+    r.table('posts').insert({ title: "hello", content: "test content", author: "test", timestamp: 1234567 }).run(conn, function(err, result) {
         if (err) throw err;
         console.log('Test post created successfully.');
+    })
+
+    r.db('cackle').tableCreate('users').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Table `users` was created successfully.');
+    })
+
+    r.table('users').insert({username:"test", password:"$2b$12$ZlYBlPfgyqTxiUtJcb0SFu9TQ5x9jlONocsuLDrogWoomSr7Az7.q"}).run(conn, function(err, result){
+        if (err) throw err;
+        console.log('Test user with password `test123` and username of `test` created successfully');
+    });
+
+
+    r.db('cackle').tableCreate('logins').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Table `logins` was created successfully.');
     })
 
 });
